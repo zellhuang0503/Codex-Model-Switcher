282eb5db921cd40048c994d3387b7a0a4e5fc28c
zellhuang0503 <zell.huang@vai-marketing.com>
2026-08-14 13:56:23 +0800

    完成 Windows 主視窗與高解析度支援

---
+++ Program.cs
@@ -0,0 +1,13 @@
+namespace CodexModelSwitcher;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainForm());
+    }
+}
